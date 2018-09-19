e7eaef51cd5487b6767897588fb846e0546c54bf
Zuber <zuberanwar@googlemail.com>
2018-09-19 17:23:03 +0100

    david yetkin zuber pairing

--- src/index.js
+++ src/index.js
@@ -2,4 +2,127 @@ function add(a, b){
   return a + b;
 }
 
-exports.add = add;
+//module.exports.add = add;
+
+function longestString(array) {
+  array.sort((a,b) => b.length-a.length);
+  return array[0];
+}
+
+//module.exports.longestString = longestString;
+
+function l337(string) {
+  return string.replace(/i/g, "1")
+  .replace(/l/g, "1")
+  .replace(/z/g, "2")
+  .replace(/e/g, "3")
+  .replace(/a/g, "4")
+  .replace(/s/g, "5")
+  .replace(/g/g, "6")
+  .replace(/t/g, "7")
+  .replace(/y/g, "7")
+  .replace(/b/g, "8")
+  .replace(/q/g, "9")
+  .replace(/o/g, "0");
+}
+
+//module.exports.l337 = l337;
+
+function uniqueStrings (arr) {
+  let newArr = [];
+  arr.forEach(item => {
+    if (!newArr.includes(item)) newArr.push(item);
+  });
+  return newArr;
+}
+
+//module.exports.uniqueStrings = uniqueStrings;
+
+function Developer (string, arr) {
+  this.name = string;
+  this.languages = arr;
+}
+
+//module.exports.Developer = Developer;
+
+
+Developer.prototype.learnLanguage = function(language) {
+  this.languages.push(language);
+  return this;
+}
+
+module.exports.learnLanguage = Developer.learnLanguage;
+
+function stringsConcat (arr) {
+  return arr.filter(item => typeof item === 'string').reduce((a,b)=>(a+b));
+}
+
+//module.exports.stringsConcat = stringsConcat;
+
+function negativeOnly (arr) {
+  return arr.filter(function(item){
+    return item < 0;
+  })
+}
+
+//module.exports.negativeOnly = negativeOnly;
+
+function camelise(string) {
+   const nameArr = string.split(" ")
+   for(let i = 1; i < nameArr.length; i++){
+     nameArr[i] = nameArr[i].charAt(0).toUpperCase() + nameArr[i].substr(1);
+   }
+   return nameArr.join("");
+}
+
+//exports.camelise = camelise;
+
+function isPrime(num) {
+  for (let i = 2; i < num; i++) {
+    if (num%i===0) return false;
+  }
+  return true;
+}
+
+function Walker () {
+  this.journey = [[0,0]];
+}
+
+
+Walker.prototype.walk = function(string, number) {
+  let lastLocation = this.journey[this.journey.length - 1];
+  if (string === 'N') this.journey.push([lastLocation[0], lastLocation[1] + number]);
+  if (string === 'S') this.journey.push([lastLocation[0], lastLocation[1] - number]);
+  if (string === 'E') this.journey.push([lastLocation[0] + number, lastLocation[1]]);
+  if (string === 'W') this.journey.push([lastLocation[0] - number, lastLocation[1]]);
+  return this;
+}
+
+module.exports.walk = Walker.walk;
+
+Walker.prototype.currentLocation = function() {
+  return this.journey[this.journey.length - 1];
+}
+
+module.exports.currentLocation = Walker.currentLocation;
+
+Walker.prototype.pathTaken = function() {
+  return this.journey;
+}
+
+module.exports.pathTaken = Walker.pathTaken
+
+Walker.prototype.totalSteps = function()
+
+module.exports = {
+  add,
+  longestString,
+  l337,
+  uniqueStrings,
+  Developer,
+  stringsConcat,
+  negativeOnly,
+  camelise,
+  isPrime,
+  Walker
+};
--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,100 @@
-const functions = require('../src/index');
+const functions = require('../src/index.js');
 
 test('Addition', function(){
   const expected = 5;
   const result = functions.add(2, 3);
-  expect(result).toBe(expected);
+  expect(result).toEqual(expected);
 });
+
+test('longestString', function() {
+  const expected = "hippopatamus";
+  const result = functions.longestString(["cat", "Dog", "a", "hippopatamus"])
+  expect(result).toEqual(expected)
+});
+
+test('l337', function() {
+  const expected = "112345677890";
+  const result = functions.l337("ilzeasgtybqo");
+  expect(result).toEqual(expected);
+});
+
+test('uniqueStr', function() {
+  const expected = ['cat','dog','hi'];
+  const result = functions.uniqueStrings(['cat','cat','dog','hi']);
+  expect(result).toEqual(expected);
+});
+
+test('Developer', function() {
+  const expected = {name: "zuber", languages: ["js", "html"]}
+  const result = new functions.Developer("zuber",["js", "html"])
+  expect(result).toEqual(expected);
+})
+
+test('learnLanguage', function(){
+  const david = new functions.Developer("david", ["js", 'html']);
+  const expected = {name: "david", languages: ["js", "html", "css"]};
+  const result = david.learnLanguage("css");
+  expect(result).toEqual(expected);
+})
+
+test('stringsConcat', function() {
+  const expected = 'hello world';
+  const result = functions.stringsConcat(['hello',' ',5,23523,'wor',9837,'ld']);
+  expect(result).toEqual(expected);
+});
+
+test('negativeOnly', function() {
+  const expected = [-1,-3423532];
+  const result = functions.negativeOnly([-1,343,-3423532, 34242, 85]);
+  expect(result).toEqual(expected);
+})
+
+test('camelise', function() {
+  const expected = "yetkinZuberDavid";
+  const result = functions.camelise("yetkin zuber david");
+  expect(result).toEqual(expected);
+});
+
+test('isPrime', function() {
+  const expected = true;
+  const result = functions.isPrime(1097);
+  expect(result).toEqual(expected);
+});
+
+test('Walker', function() {
+  const expected = {journey:[[0,0]]};
+  const result = new functions.Walker();
+  expect(result).toEqual(expected);
+})
+
+test('walk', function() {
+  const david = new functions.Walker();
+  // const expected = {journey:[[0, 0], [0, 12]]};
+  const expected2 = {journey: [[0,0],[0,12],[-6, 12]]};
+  david.walk("N", 12);
+  const result2 = david.walk("W", 6);
+  // expect(result).toEqual(expected);
+  expect(result2).toEqual(expected2);
+});
+
+test('currentLocation', function() {
+  const yetkin = new functions.Walker();
+  yetkin.walk("N",5);
+  yetkin.walk("S",2);
+  yetkin.walk("W",9);
+  yetkin.walk("E",7);
+  const expected = [-2,3];
+  const result = yetkin.currentLocation();
+  expect(result).toEqual(expected);
+});
+
+test('pathTaken', function() {
+  const zuber = new functions.Walker();
+  zuber.walk("N",5);
+  zuber.walk("S",2);
+  zuber.walk("W",9);
+  zuber.walk("E",7);
+  const expected = [[0,0],[0,5],[0,3],[-9,3],[-2,3]];
+  const result = zuber.pathTaken();
+  expect(result).toEqual(expected);
+})
